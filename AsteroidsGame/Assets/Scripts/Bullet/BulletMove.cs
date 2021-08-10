b52599de6ec65442ca21de00078712957b484af7
Nemti <nemti166@gmail.com>
2021-08-10 16:17:25 +0300

    Rename bullet move

--- AsteroidsGame/Assets/Scripts/Bullet/BulletMove.cs
+++ AsteroidsGame/Assets/Scripts/Bullet/BulletMove.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletMove : MonoBehaviour
+namespace Game.Helper
 {
-    // Start is called before the first frame update
-    void Start()
+    public class BulletMove : Bullet
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        protected override void MoveBullet(Vector2 direction, float speed, Rigidbody2D rigidbody)
+        {
+            rigidbody.velocity = direction * speed;
+        }
     }
 }
